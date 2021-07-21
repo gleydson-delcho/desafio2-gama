8a9c1acd1b8d16e8c30ca189bc2a61fb590d2100
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-07-20 21:54:10 -0300

    Create, clean and estruture the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
+import Clientes from './pages/Clientes';
+import Home from './pages/Home';
+import Produtos from './pages/Produtos';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/clientes" component={Clientes} />
+      <Route exact path="/produtos" component={Produtos} />
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    return(
+        <>
+        <h1>Home</h1>
+        <a href="/clientes">Clientes</a>
+        <a href="/produtos">Produtos</a>
+        </>
+    );
+}
