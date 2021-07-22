610c4359e1db4becc15763cbd965a80f1477e161
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-07-22 19:31:22 -0300

    Update link routes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.module.scss';
+
 
 export default function Navbar(): JSX.Element {
     return (
         <aside>
-            <a href="/">Home</a>
-            <a href="/clientes">Clientes</a>
-            <a href="/produtos">Produtos</a>
+            <Link  to="/">Home</Link>
+            <Link to="/clientes">Clientes</Link>
+            <Link to="/produtos">Produtos</Link>
         </aside>
     );
 }
